325e2f3e27bc9352665fed7ccb9e75f76c309212
bszymala <bszymala20@gmail.com>
2025-11-03 18:59:54 +0100

    added self counting id system

--- models/customer.py
+++ models/customer.py
@@ -1,7 +1,16 @@
 class customer:
-    def __init__(self, id, name):
-        self.id = id
+    _countID = 0
+
+    def __init__(self, name, isRentingBike = False, isRentingHelmet = False, id = None):
+        if id == None:
+            customer._countID += 1
+            self.id = customer._countID
+        else:
+            self.id = id
         self.name = name
-        self.isRentingBike = False
-        self.isRentingHelmet = False
+        self.isRentingBike = isRentingBike
+        self.isRentingHelmet = isRentingHelmet
+
+    def __str__(self):
+        return ", Customer Name: " + self.name + ", Customer ID: " + str(self.id)
  
